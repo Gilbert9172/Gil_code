5504d507f3db982962a01c8ecbde9cd04d235f8c
gilbert9172 <gilbert950904@gmail.com>
2021-08-13 22:46:36 +0900

    algo

--- 백준문제풀이/2주차/210815-08.py
+++ 백준문제풀이/2주차/210815-08.py
@@ -1,4 +1,4 @@
-# 더하기 사이클 (푸는 중)
+# 더하기 사이클 (푸는 중 -> 스터디 때 같이)
 
 """
 99 >= x >= 0 
@@ -6,41 +6,41 @@
 먼저 주어진 수가 10보다 작다면 앞에 0을 붙여 두자리로 만든다.
 그 다음, 주어진 수의 가장 오른쪽 자리 수와 앞에서 구한 합의 가장 오른쪾 자리수를
 """
-# a = '14'
-# print(a[1])
 
-# b = int(a[0])+int(a[1])
-# print(b)
+"""
+정수 N의 앞자리 수 : N[0]
+정수 N의 뒷자리 수 : N[-1]
 
-# c = (int(a[1])*10)+b
-# print(c)
+새로 생성되는 숫자 : N1 = N[0]+N[-1]
+새로 생성되는 숫자의 뒷자리 : N1[-1]
 
+또 생성되는 숫자 : N = N[-1]*10 + N1[-1]
+"""
 
-# N의 뒷자리 : N[-1]
-# "N의 각 자리수의 합"의 뒷자리 : (N[0]+N[-1])[-1]
+N = input()
+n = int(N)
 
-# 새로운 숫자 : ( N[-1]*10 ) + (N[0]+N[-1])[-1]
+if n < 10:
+    n1 = int(N) * 11
+else:
+    n1 = int(N[-1])*10 + int(str(int(N[0])+int(N[-1]))[-1])
 
-# new_num == N -> break
+count=1
+while n1 != n:
+    n1 = int(str(n1)[-1])*10 + int(str(int(str(n1)[0])+int(str(n1)[-1]))[-1])
+    count += 1
 
-N = int(input())
+    if n1 < 10:
+        n1 = n1 * 11
+        count += 1
+        continue
 
-count = 0
-# while True:
-#     # while N < 10:
-#     #     N *= 10
-
-#     n = str(N)
-#     n1 = (int(n[-1])*10) + int((n[0]+n[-1])[-1])
-#     count += 1
-#     if N == n1:
-#         break
-# print(count)
-while True:
-    try:
-        n = str(N)
-        n1 = (int(n[-1])*10) + int((n[0]+n[-1])[-1])
-        if n1 == N:
-            count += 1
-    except:
+    elif n1 == n:
         break
+    
+
+print(count)
+
+
+
+
