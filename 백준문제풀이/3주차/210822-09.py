5205aad0867c4761b3818eb988e59775acae3e69
gilbert9172 <gilbert950904@gmail.com>
2021-08-19 06:21:26 +0900

    셀프 넘버

--- 백준문제풀이/3주차/210822-09.py
+++ 백준문제풀이/3주차/210822-09.py
@@ -11,7 +11,27 @@
 10000보다 작거나 같은 셀프 넘버를 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
 """
 
-# 우선 생성을 할 수 있는 함수를 만들기.
+# 생성할 수 있는 수를 만들고 차집합을 구해보기.
+
+#첫 번째 코드 
+lst = []
+for i in range(1,10001):    
+    if i < 10:
+        lst.append(i+i)
+    elif 10<=i<100:
+        lst.append(i+ i//10 + i%10)
+    elif 100<=i<1000:
+        lst.append(i+ i//100 + (i%100)//10 + ((i%100)%10)//1)
+    elif 1000<=i<10000:
+        lst.append(i + i//1000 + (i%1000)//100 + ((i%1000)%100)//10 + (((i%1000)%100)%10)//1)
+
+
+for i in range(1,10001):
+    if i not in lst:
+        print(i)
+
+
+# 두 번째 코드
 def d(n):
     n = n + sum([int(i) for i in str(n)])     # 각 자리수 더하기 
     return n
