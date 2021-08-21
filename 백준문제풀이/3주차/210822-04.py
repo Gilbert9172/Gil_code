6238765a658484cb814245efe213dbd3bf787103
Gilbert Jung <83274792+Gilbert9172@users.noreply.github.com>
2021-08-22 01:53:32 +0900

    나머지

--- 백준문제풀이/3주차/210822-04.py
+++ 백준문제풀이/3주차/210822-04.py
@@ -14,7 +14,7 @@
 수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 
 
 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
-"""
+""" 
 import sys
 num = [int(sys.stdin.readline())%42 for i in range(10)]
 print(len(set(num)))
