fc5755843e74a7d1af08cb77d3d42a5e27e20899
gilbert9172 <gilbert950904@gmail.com>
2021-08-23 12:56:39 +0900

    숫자의 합

---
+++ 백준문제풀이/4주차/210829-02.py
@@ -0,0 +1,24 @@
+"""
+문제번호 : 11720
+
+단계 : 	문자열
+
+제목 : 숫자의 합
+
+문제 : N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+
+알고리즘 분류 : 수학/문자열/사칙연산.
+"""
+
+N = int(input())
+
+for i in range(1):
+    data = input()
+
+    plus = sum(map(int,data))
+
+print(plus)
+
+# ???의 코드 
+N = input()
+print(sum(map(int,N)))
