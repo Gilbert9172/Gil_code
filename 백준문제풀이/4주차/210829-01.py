fdbccf91ce1a58c5263259941ac3b75c181c2520
gilbert9172 <gilbert950904@gmail.com>
2021-08-23 12:43:36 +0900

    아스키코드

--- 백준문제풀이/4주차/210829-01.py
+++ 백준문제풀이/4주차/210829-01.py
@@ -12,13 +12,11 @@
 # chr(숫자) : 숫자에 맞는 아스키 코드를 반환해 준다.
 
 # 코드 (정규표현식 사용)
-# import re 
-# p = re.compile('[a-zA-Z0-9]')  
-# N = input()
-# m = p.match(N)
-# if m:
-#     print(ord(N))
-# else:
-#     print('No match')
-
-print(ord('9'))
+import re 
+p = re.compile('[a-zA-Z0-9]')  
+N = input()
+m = p.match(N)
+if m:
+    print(ord(N))
+else:
+    print('No match')
