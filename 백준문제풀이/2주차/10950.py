e237bd498ea1d956d3ebc2244922828dab6deace
gilbert9172 <gilbert950904@gmail.com>
2021-08-30 13:23:27 +0900

    for문 복습

--- 백준문제풀이/2주차/10950.py
+++ 백준문제풀이/2주차/10950.py
@@ -6,3 +6,7 @@ for i in range(N):
     data = list(map(int,sys.stdin.readline().split()))
     a = sum(data)
     print(a)
+
+for i in range(N):
+    data = sum(map(int,sys.stdin.readline().split()))
+    print(data)
