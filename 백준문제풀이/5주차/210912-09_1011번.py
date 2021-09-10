a25a9df15c7fa07b65217f5891d46e8e3f3dcef3
gilbert9172 <gilbert950904@gmail.com>
2021-09-11 00:16:46 +0900

    Fly me to the Alpha Centauri

--- 백준문제풀이/5주차/210912-09_1011번.py
+++ 백준문제풀이/5주차/210912-09_1011번.py
@@ -5,3 +5,27 @@
 알고리즘 분류 : 수학
 """
 
+# import math
+# for _ in range(int(input())):
+#     x, y = map(int, input().split())
+#     print(math.ceil(2 * math.sqrt(y - x) - 1))
+
+
+t = int(input())
+for i in range(t):
+
+    a, b = map(int, input().split())
+    c = b - a
+
+    num = 1
+    while True:
+        if num ** 2 <= c < (num + 1) ** 2:
+            break
+        num += 1
+        
+    if num ** 2 == c:
+        print((num * 2) - 1)
+    elif num ** 2 < c <= num ** 2 + num:
+        print(num * 2)
+    else:
+        print((num * 2) + 1)
